cea28d97227d0df5c3338424bb9cc441e0af924b
bernardo <epiphonetodie@gmail.com>
2018-11-15 16:27:14 +0100

    sorting, towerofhanoi

---
+++ HelloWorld/Sorting.fs
@@ -0,0 +1,21 @@
+﻿module Sorting
+
+open System.Linq.Expressions
+
+let rec quickSort = function
+    | [] -> []
+    | n::ns ->  let lessthan, greaterEqual = List.partition((>) n) ns
+                quickSort lessthan @ n :: quickSort greaterEqual
+
+let rand = new System.Random()
+let data = List.init 10 (fun _ -> rand.Next())
+let result = quickSort data
+let result2 = List.sort data
+
+let rec quickSort_func (pxs:seq<_>) =
+    seq {
+        match Seq.toList pxs with
+        | p::xs ->  let lessthan, greaterEqual = List.partition ((>=) p) xs
+                    yield! quickSort_func lessthan; yield p; yield! quickSort_func greaterEqual
+        | _ -> ()
+    }
--- HelloWorld/TowerOfHanoi.fs
+++ HelloWorld/TowerOfHanoi.fs
@@ -1,18 +1,22 @@
 ﻿module TowerOfHanoi
 
-let rec TowerOfHanoi f x t n =
-    if n > 0 then
-        TowerOfHanoi f t x (n - 1)
-        printfn "Move disc from %c to %c" f t
-        TowerOfHanoi x f t (n - 1)
+let rec TowerOfHanoi from middle destiny number =
+    if number > 0 then
+        TowerOfHanoi from destiny middle (number - 1)
+        printfn "Move disc %d from %c to %c" (number - 1) from destiny
+        TowerOfHanoi middle from destiny (number - 1)
 
-let rec hanoi n o d t =
-    if n = 0 then [] else
-        (hanoi (n - 1) o t d) @ [(o, d)] @ (hanoi (n - 1) t d o)
+let rec hanoi number origin destination extra =
+    if number = 0 then [] else
+        (hanoi (number - 1) origin extra destination) @ [(origin, destination)] @ (hanoi (number - 1) extra destination origin)
 
-let rec TowerOfHanoiRec n s f =
-    match n with
+let rec TowerOfHanoiRec number start final =
+    match number with
     | 0 -> []
     | _ -> 
-        let t = (6 - s - f)
-        (TowerOfHanoiRec (n - 1) s t) @ [s, f] @ (TowerOfHanoiRec(n - 1) t f)
+        let trail = (6 - start - final)
+        let list = (TowerOfHanoiRec (number - 1) start trail) @ [start, final] @ (TowerOfHanoiRec(number - 1) trail final)
+        //printfn "%O" list
+        list
+
+(TowerOfHanoiRec 3 1 3) |> List.iter (fun (x, y) -> printfn "Move the disc from %A to %A" x y);;
